ee3e237106ea82434bfe8d0be2e5d94206cccd17
Burcu Noyan <burcunoyan@gmail.com>
2017-10-08 15:42:41 -0400

    Productmust have title, description, price, inventory quantity. Product will have photo

--- db/Product.js
+++ db/Product.js
@@ -4,9 +4,24 @@ const conn = require('./conn');
 const Product = conn.define('product', {
   name: {
     type: Sequelize.STRING,
-    validate: {
-      notEmpty: true
-    }
+    allowNull: false,
+    validate: { notEmpty: { msg: 'Product name is required.' }}
+  },
+  description: {
+    type: Sequelize.TEXT,
+    validate: { notEmpty: { msg: 'Product description is required.' }}
+  },
+  price: {
+    type: Sequelize.FLOAT,
+    validate: { notEmpty: { msg: 'Price is required.' }}
+  },
+  inventoryQuantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 0
+  },
+  photo: {
+    type: Sequelize.STRING
   }
 });
 
