b5937c6991634780acdfa10749da968951255ba1
Wasif Zaman <teatocode@gmail.com>
2017-10-09 08:54:27 -0400

    change db name lineItem to lineitem

--- db/LineItem.js
+++ db/LineItem.js
@@ -1,7 +1,7 @@
 const conn = require('./conn');
 const Sequelize = conn.Sequelize;
 
-const LineItem = conn.define('lineItem', {
+const LineItem = conn.define('lineitem', {
   quantity: {
     type: Sequelize.INTEGER,
     defaultValue: 1
