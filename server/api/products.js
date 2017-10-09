ba1c541ee3f76ae87d1aa10b3a366b15a32155f5
Burcu Noyan <burcunoyan@gmail.com>
2017-10-08 20:49:53 -0400

    change variable name to prevent conflict

--- server/api/products.js
+++ server/api/products.js
@@ -1,16 +1,16 @@
-const products = require('express').Router();
-const { Product } = require('../../db').models
+const router = require('express').Router();
+const { Product } = require('../../db').models;
 
-products.get('/', (req, res, next) => {
+router.get('/', (req, res, next) => {
   Product.findAll()
     .then(products => res.send(products))
     .catch(next)
 });
 
-products.get('/:id', (req, res, next) => {
+router.get('/:id', (req, res, next) => {
   Product.findById(req.params.id)
     .then(product => res.send(product))
     .catch(next)
-})
+});
 
-module.exports = products;
+module.exports = router;
