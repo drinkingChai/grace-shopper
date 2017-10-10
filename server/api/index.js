a0a4880dd8f7b8361e02591e5dd2fb73fae673cc
Wasif Zaman <teatocode@gmail.com>
2017-10-10 19:09:37 -0400

    change sessions to get checkSession

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const sessions = require('./middlewares').sessions
+const { checkSession } = require('./middlewares').sessions
 
 router.use('/products', require('./products'));
-router.use('/orders', sessions.checkSession, require('./orders'));
+router.use('/orders', checkSession, require('./orders'));
 router.use('/users', require('./users'));
 router.use('/sessions', require('./sessions'));
 
