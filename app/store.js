66d6dcaf878797bdf453122de9c19ce14d660aeb
Burcu Noyan <burcunoyan@gmail.com>
2017-10-09 22:02:22 -0400

    add GET_ORDERS code to store

--- app/store.js
+++ app/store.js
@@ -3,13 +3,9 @@ import thunkMiddleware from 'redux-thunk';
 import loggerMiddleware from 'redux-logger';
 import axios from 'axios';
 
-// INITIAL STATE
-const initialState = {
-  products: []
-};
-
 // ACTION NAMES
 const GET_PRODUCTS = 'GET_PRODUCTS';
+const GET_ORDERS = 'GET_ORDERS';
 
 // ACTION CREATORS
 const getProducts = (products) => {
@@ -19,19 +15,44 @@ const getProducts = (products) => {
   }
 };
 
+const getOrders = (orders) => {
+  return {
+    type: GET_ORDERS,
+    orders
+  }
+};
+
 // THUNKS
 export const fetchProducts = () => dispatch => {
   axios.get('/api/products')
     .then(res => res.data)
     .then(products => dispatch(getProducts(products)))
-    .catch(err => console.error('Fetching products unsuccessful', err));
+    .catch(err => console.error('Fetching products unsuccessful.', err));
 }
+
+export const fetchOrders = () => dispatch => {
+  axios.get('/api/orders')
+    .then(res => res.data)
+    .then(orders => dispatch(getOrders(orders)))
+    .catch(err => console.error('Fetching orders unsuccessful.', err));
+};
+
+
+// INITIAL STATE
+const initialState = {
+  products: [],
+  orders: []
+};
 
 // REDUCER
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+
     case GET_PRODUCTS:
       return Object.assign({}, state, { products: action.products });
+
+    case GET_ORDERS:
+      return Object.assign({}, state, {orders: action.orders})
 
     default:
       return state;
