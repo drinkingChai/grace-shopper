fbdb7af39ed423c827cbd174ab27f5164b5b4e05
Wasif Zaman <teatocode@gmail.com>
2017-10-10 11:38:36 -0400

    create user api

---
+++ server/api/users.js
@@ -0,0 +1,10 @@
+const users = require('express').Router()
+const { User } = require('../../db').models
+
+users.post('/', (req, res, next) => {
+  User.create(req.body)
+    .then(() => res.sendStatus(202))
+    .catch(next)
+})
+
+module.exports = users
