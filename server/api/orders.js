4cf3ae85616c7416769d470384bb93bceeced43a
Burcu Noyan <burcunoyan@gmail.com>
2017-10-09 22:17:26 -0400

    update file

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,41 +1,41 @@
-const orders = require('express').Router();
+const router = require('express').Router();
 const { Order, LineItem, Product } = require('../../db').models
 
-orders.get('/', (req, res, next) => {
+router.get('/', (req, res, next) => {
   Order.findAll({
     include: [{ model: LineItem, include: [ Product ] }]
   })
     .then(orders => res.send(orders))
     .catch(next)
-})
+});
 
-orders.get('/:id', (req, res, next) => {
+router.get('/:id', (req, res, next) => {
   Order.findById(req.params.id, { include: [{ model: LineItem, include: [ Product ] }] })
     .then(order => res.send(order))
     .catch(next)
-})
+});
 
-orders.put('/products/:productId', (req, res, next) => {
+router.put('/products/:productId', (req, res, next) => {
   // for DB team - to be replaced with findCart to reduce logic in api
   Order.findOne({
     where: { isCart: true },
     include: [ LineItem ]
   })
     .then(order => {
-      if (order) return order
-      return Order.create()
+      if (order) return order;
+      return Order.create();
     })
     .then(order => {
-      let lineItem = order.lineitems && order.lineitems.find(li => li.productId == req.params.productId) ||
-        LineItem.build({ orderId: order.id, productId: req.params.productId })
-      Object.assign(lineItem, req.body)
-      return lineItem.save()
+      let lineItem = order.lineitems && order.lineitems.find(li => li.productId === req.params.productId) ||
+        LineItem.build({ orderId: order.id, productId: req.params.productId });
+      Object.assign(lineItem, req.body);
+      return lineItem.save();
     })
     .then(() => res.sendStatus(201))
     .catch(next)
-})
+});
 
-orders.delete('/:id/products/:productId', (req, res, next) => {
+router.delete('/:id/products/:productId', (req, res, next) => {
   // for DB team - to be replaced to reduce logic in api
   Order.findOne({
     where: { id: req.params.id },
@@ -46,12 +46,12 @@ orders.delete('/:id/products/:productId', (req, res, next) => {
   })
     .then(order => {
       if (order) {
-        order.lineitems[0].destroy()
-        return res.sendStatus(200)
+        order.lineitems[0].destroy();
+        return res.sendStatus(200);
       }
-      res.sendStatus(404)
+      res.sendStatus(404);
     })
     .catch(next)
-})
+});
 
-module.exports = orders
+module.exports = router;
