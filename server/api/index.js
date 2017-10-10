a17a8f622c8a66b021c5de097610d6d9bea600eb
Wasif Zaman <teatocode@gmail.com>
2017-10-10 13:01:51 -0400

    add session-middleware to orders api

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
+const sessions = require('./middlewares').sessions
 
 router.use('/products', require('./products'));
-router.use('/orders', require('./orders'));
+router.use('/orders', sessions.checkSession, require('./orders'));
 router.use('/users', require('./users'));
 router.use('/sessions', require('./sessions'));
 
