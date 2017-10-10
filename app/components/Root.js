374ab775a9e77ec26f19a1db35edaf9311ffa647
Burcu Noyan <burcunoyan@gmail.com>
2017-10-09 22:03:29 -0400

    add fetchOrders and Cart component to Root

--- app/components/Root.js
+++ app/components/Root.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import store, { fetchProducts } from '../store';
+import store, { fetchProducts, fetchOrders } from '../store';
 
 // Components
 import Nav from './Nav';
 import AllProducts from './AllProducts';
+import Cart from './Cart';
 
 export default class Root extends Component {
   componentDidMount () {
     store.dispatch(fetchProducts());
+    store.dispatch(fetchOrders());
   }
 
   render () {
@@ -18,12 +20,14 @@ export default class Root extends Component {
         <Route component={ Nav } />
         <main className="container">
           <h1 style={{ marginBottom: '30px' }}>Welcome to GraceShopper!</h1>
-          <div className="row">
+          <div className="col-xs-12 col-sm-8">
             <Switch>
               <Route exact path="/products" component={ AllProducts } />
-              {/* <Route component={ Cart } /> */}
               <Redirect exact path="/" to="/products" />
             </Switch>
+          </div>
+          <div className="col-xs-12 col-sm-4">
+            <Cart />
           </div>
         </main>
       </div>
