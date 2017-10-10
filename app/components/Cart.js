8f07194ba4b4dd01770f0ff15dada521a19ab83a
Wasif Zaman <teatocode@gmail.com>
2017-10-10 00:18:57 -0400

    show total price and add check-out

--- app/components/Cart.js
+++ app/components/Cart.js
@@ -1,27 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { checkOut } from '../store';
 import CartUpdateForm from './CartUpdateForm';
 
-const Cart = ({ order }) => {
-  if (!order) return <div />;
+const Cart = (props) => {
+  const {order} = props;
+
+  if (!order) return <div></div>
+
+  const total = order.lineitems.reduce((total, item)=> (total += item.product.price * item.quantity), 0);
 
   return (
+
     <div className="panel panel-default">
-      <h3 className="panel-heading" style={{ margin: 0 }}>
+      <div className="panel-heading">
         Your Cart
-      </h3>
+      </div>
       <div className="panel-body">
-        {/* enter Order Lines Here */}
-        { order.lineitems.map(lineitem => <CartUpdateForm key={ lineitem.id } lineitem={ lineitem } />) }
+      {/* enter Order Lines Here */}
+      { order.lineitems.map(lineitem => <CartUpdateForm key={ lineitem.id } lineitem={ lineitem }/>) }
+      <hr/>
+      <form onSubmit={ props.placeOrder }>
+        Total: { total }
+        <button className='pull-right'>Checkout</button>
+      </form>
       </div>
     </div>
   )
-};
+}
 
 const mapStateToProps = (state) => {
   return {
     order: state.orders.find(order => order.isCart === true)
   }
-};
+}
 
-export default connect(mapStateToProps)(Cart);
+const mapDispatch = dispatch => ({
+  placeOrder(ev) {
+    ev.preventDefault();
+    dispatch(checkOut());
+  }
+})
+
+export default connect(mapStateToProps, mapDispatch)(Cart);
