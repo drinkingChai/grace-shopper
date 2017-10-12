8171d69e4b975667696f6d56716ecea2425e2ae4
Dan Weissbard <danweissbard@gmail.com>
2017-10-12 19:14:12 -0400

    Store has been split

---
+++ app/store/cart.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { fetchOrders } from './orders';
+
+const ADD_TO_CART = 'ADD_TO_CART';
+
+export const updateCartItem = (product, quantity) => dispatch =>
+  axios.put(`/api/orders/products/${product.id}`, { quantity, price: product.price })
+    .then(() => dispatch(fetchOrders()))
+    .catch(err => console.log(err.message))
+
+export const checkOut = () => dispatch =>
+  axios.put('/api/orders/check-out')
+    .then(() => dispatch(fetchOrders()))
+    .catch(err => console.log(err.message))
+
+const initialState = {}
+
+const reducer = (state= initialState, action) =>{
+
+  switch (action.type){
+    default:
+      return state
+  }
+}
+
+export default reducer;
---
+++ app/store/currentUser.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import {getOrders, fetchOrders} from './orders';
+
+const LOGIN = 'LOGIN'
+const LOGOUT = 'LOGOUT'
+
+const login = user => ({ type: LOGIN, user })
+const logout = () => ({ type: LOGOUT })
+
+export const checkSession = () => dispatch =>
+  axios.get('/api/sessions')
+    .then(res => dispatch(login(res.data)))
+    .then(() => dispatch(fetchOrders()))
+    .catch(err => console.log(err.message))
+
+export const loginUser = (email, password) => dispatch =>
+  axios.put('/api/sessions', { email, password })
+    .then(() => dispatch(checkSession()))
+    .catch(err => console.log(err.message))
+
+export const logoutUser = () => dispatch =>
+  axios.delete('/api/sessions')
+    .then(() => dispatch(logout()))
+    .then(() => dispatch(getOrders([])))
+    .catch(err => console.log(err.message))
+
+export const registerUser = userData => dispatch =>
+  axios.post('/api/users', userData)
+    .then(res => dispatch(loginUser(res.data)))
+    .catch(err => console.log(err.message))
+
+
+
+
+const reducer = (currentUser = {}, action) => {
+  switch (action.type) {
+    case LOGIN:
+      return action.user
+    case LOGOUT:
+      return {}
+    default:
+      return currentUser;
+  }
+};
+
+export default reducer;
