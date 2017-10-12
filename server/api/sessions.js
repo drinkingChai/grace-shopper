4c211a2627484995f4441235a8b5f149b5bb0e27
Burcu Noyan <burcunoyan@gmail.com>
2017-10-12 15:21:47 -0400

    add findSession; need to create session if no session

--- server/api/sessions.js
+++ server/api/sessions.js
@@ -2,17 +2,12 @@ const sessions = require('express').Router();
 const { Session, User } = require('../../db').models;
 
 sessions.get('/', (req, res, next) => {
-  if (req.session && req.session.id) {
-    Session.findById(req.session.id)
-      .then(session => {
-        if (!session) {
-          delete req.session.id
-          // can change sendStatus to status(200).send('session not found')?
-          return res.sendStatus(401)
-        }
-        res.send(session.data);
-      })
-  } else res.sendStatus(401)
+  Session.findSession(req.session.id)
+    .then(session => {
+      if (!session) return res.sendStatus(401);
+      res.send(session.data);
+    })
+    .catch(next)
 })
 
 sessions.put('/', (req, res, next) => {
@@ -26,7 +21,7 @@ sessions.put('/', (req, res, next) => {
           name: user.name,
           email: user.email
         }
-        
+
         const sessionData = user.sessions.find(sess => sess.isActive) || Session.build({ userId: user.id });
         Object.assign(sessionData, { data: req.session.data });
 
