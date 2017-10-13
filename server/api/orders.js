afb813eabd027e9ad05807f1a44003ef73523634
Wasif Zaman <teatocode@gmail.com>
2017-10-13 01:11:39 -0400

    put new cart and update on req.session

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const { Order, LineItem, Product } = require('../../db').models;
+const { Order, LineItem, Product } = require('../db').models;
 const options = [{ model: LineItem, include: [ Product ] }];
 
 router.get('/', (req, res, next) => {
   Order.findAll({
-    where: { userId: req.session.data.userId },
+    where: { userId: req.session.userId },
     include: options,
     order: [[ LineItem, 'createdAt', 'ASC' ]]
   })
@@ -19,24 +19,28 @@ router.get('/:id', (req, res, next) => {
 });
 
 router.get('/filter', (req, res, next) => {
-  Order.findAll({ where: { userId: req.session.data.userId, status: req.query.status }})
+  Order.findAll({ where: { userId: req.session.userId, status: req.query.status }})
     .then(orders => res.send(orders))
     .catch(next)
 });
 
 router.put('/check-out', (req, res, next) => {
-  Order.checkOut(req.session.data.userId)
-    .then(order => {
-      if (!order) return res.sendStatus(404);
+  Order.checkOut(req.session.userId, req.body)
+    .then(newCart => {
+      req.session.cart = newCart
       res.sendStatus(201);
     })
     .catch(next);
 });
 
 router.put('/products/:productId', (req, res, next) => {
-    Order.updateCart(req.session.data.userId, req.params.productId * 1, req.body)
-      .then(() => res.sendStatus(201))
-      .catch(next);
+  Order.updateCart(req.session.userId, req.params.productId * 1, req.body)
+    .then(() => Order.findCart(req.session.userId))
+    .then(cart => {
+      req.session.cart = cart
+      res.sendStatus(201)
+    })
+    .catch(next);
 });
 
 router.delete('/:id/products/:productId', (req, res, next) => {
