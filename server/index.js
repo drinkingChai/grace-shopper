49f7fea86766f86cced3042929cc99ac366270ca
Wasif Zaman <teatocode@gmail.com>
2017-10-13 01:08:07 -0400

    add default session

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const session = require('client-sessions')
-const db = require('../db');
+const db = require('./db');
 
 const app = express();
 
@@ -19,11 +19,11 @@ app.use(session({
   secret: process.env.SESSIONSECRET,
   maxAge: 30 * 60 * 1000
 }))
-// app.use((req, res, next) => {
-//   // session logger
-//   console.log('session', req.session)
-//   next()
-// })
+app.use((req, res, next) => {
+  req.session.cart = req.session.cart || db.models.Order.build()
+  req.session.cart.lineitems = req.session.cart.lineitems || []
+  next()
+})
 app.use('/api', require('./api'));
 app.get('/*', (req, res, next) => {
   res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
