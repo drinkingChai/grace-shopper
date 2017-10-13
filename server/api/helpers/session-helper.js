faca49e0f8d3eec0fa8bbf6b212fac73e2f03e5f
Wasif Zaman <teatocode@gmail.com>
2017-10-13 17:05:54 -0400

    some error handling

--- server/api/helpers/session-helper.js
+++ server/api/helpers/session-helper.js
@@ -1,14 +1,13 @@
 const { Product, LineItem } = require('../../db').models
 
 const updateSessionCart = (cart, productId, updateData) => {
-  // Promise resolve to make it thenable
-  if (updateData.quantity <= 0) return Promise.resolve(cart.lineitems.filter(li => li.productId != productId))
-
   return Product.findById(productId)
     .then(product => {
-      if (!product) return res.sendStatus(404)
+      if (!product) return Promise.reject('Product not found')
 
-      // build lineitem object //
+      if (updateData.quantity <= 0) return cart.lineitems.filter(li => li.productId != productId)
+
+      // ==== build lineitem object ==== //
       let lineItem = LineItem.build({ productId: product.id });
       Object.assign(lineItem, updateData);
 
@@ -16,7 +15,7 @@ const updateSessionCart = (cart, productId, updateData) => {
         without serialization and deserialization */
       lineItem = JSON.parse(JSON.stringify(lineItem))
       lineItem.product = product
-      // ==== //
+      // =============================== //
 
       // find or push
       const liIndex = cart.lineitems.findIndex(li => li.productId == product.id)
