0756058ef9393fb981ab88319d3f1cc04cd55a72
Wasif Zaman <teatocode@gmail.com>
2017-10-13 01:14:09 -0400

    checkSession empty

--- server/api/middlewares/sessions-middleware.js
+++ server/api/middlewares/sessions-middleware.js
@@ -1,16 +1,5 @@
-const { Session } = require('../../db').models
-
 const checkSession = (req, res, next) => {
-  Session.findById(req.session.id)
-    .then(session => {
-      if (!session) {
-        delete req.session.id;
-        delete req.session.data;
-        res.sendStatus(401)
-      }
-      req.session.data = session.data;
-      next()
-    })
+  next()
 }
 
 module.exports = {
