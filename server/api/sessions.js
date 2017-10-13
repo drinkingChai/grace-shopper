f87f3edc38a7a6af69d7d489cd24cfe83616d3d9
Wasif Zaman <teatocode@gmail.com>
2017-10-13 01:12:34 -0400

    on login, add cart info to session

--- server/api/sessions.js
+++ server/api/sessions.js
@@ -1,44 +1,28 @@
-const sessions = require('express').Router();
-const { Session, User } = require('../../db').models;
+const router = require('express').Router()
+const { User, Order, LineItem } = require('../db').models
 
-sessions.get('/', (req, res, next) => {
-  Session.findSession(req.session.id)
-    .then(session => {
-      if (!session) return res.sendStatus(401);
-      res.send(session.data);
-    })
-    .catch(next)
-});
-
-sessions.put('/', (req, res, next) => {
-  const { email, password } = req.body
-  User.findOne({ where: { email, password }, include: [ Session ]})
-    .then(user => {
-      if (user) {
-        // data to store
-        req.session.data = {
-          userId: user.id,
-          name: user.name,
-          email: user.email
-        }
-
-        const sessionData = user.sessions.find(sess => sess.isActive) || Session.build({ userId: user.id });
-        Object.assign(sessionData, { data: req.session.data });
-
-        return sessionData.save()
-          .then(session => {
-            req.session.id = session.id
-            res.sendStatus(202)
-          })
-      } else next();
-    })
-    .catch(next);
+router.get('/', (req, res, next) => {
+  res.send(req.session)
 })
 
-sessions.delete('/', (req, res, next) => {
-  return Session.deleteSession(req.session.id)
-    .then(() => res.sendStatus(201))
-    .catch(next)
-});
+router.put('/', (req, res, next) => {
+  const { email, password } = req.body
+  User.findOne({ where: { email, password } })
+    .then(user => {
+      if (!user) return res.sendStatus(401)
+      Order.findCart(user.id)
+        .then(cart => {
+          req.session.userId = user.id
+          req.session.cart = cart 
+          res.sendStatus(202)
+        })
+    })
+})
 
-module.exports = sessions
+router.delete('/', (req, res, next) => {
+  delete req.session.userId
+  req.session.cart = Order.build()
+  res.sendStatus(200)
+})
+
+module.exports = router
