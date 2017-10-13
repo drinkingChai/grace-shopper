2bd150d3bf166a9b529c1b5253317ee5bf5b2d1f
Wasif Zaman <teatocode@gmail.com>
2017-10-13 15:18:36 -0400

    removed require session

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { checkSession } = require('./middlewares').sessions
 
 router.use('/products', require('./products'));
 router.use('/orders', require('./orders'));
