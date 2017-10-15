94fd701598f77cb0be099efeb37a6699b55846b9
Wasif Zaman <teatocode@gmail.com>
2017-10-15 17:15:57 -0400

    bugfix: default render without product

--- client/components/Product.js
+++ client/components/Product.js
@@ -5,6 +5,8 @@ const Product = ({product, currentUser}) => {
 
   // const product = products.filter(product => product)
   console.log(product);
+  if (!product) return <div>No product here!</div>
+
   return (
     <div className="col-sm-6">
       <h2> {product.name} </h2>
