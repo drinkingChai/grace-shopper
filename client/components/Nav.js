2f6af5331755ab279b80f420132be2969b42177a
Wasif Zaman <teatocode@gmail.com>
2017-10-16 17:15:15 -0400

    nav has logout

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { logoutUser } from '../store'
 
-const Nav = ({ currentUser }) => {
+const Nav = ({ currentUser, logoutUser }) => {
   return (
     <nav className="navbar navbar-default">
       <div className='container-fluid'>
@@ -12,8 +13,10 @@ const Nav = ({ currentUser }) => {
         <ul className="nav navbar-nav pull-right">
           <li><Link to="#">Search <span className="glyphicon glyphicon-search" /></Link></li>
           { currentUser.userId ?
-            <li><Link to="/account">Account</Link></li> :
-            <li><Link to="#">Log In</Link></li> }
+            <li><Link to="/account">My Account</Link></li> :
+            <li><Link to="/login">Log In</Link></li> }
+          { currentUser.userId ?
+            <li><Link to="/" onClick={ logoutUser }>Log out</Link></li> : null }
         </ul>
       </div>
     </nav>
@@ -21,5 +24,6 @@ const Nav = ({ currentUser }) => {
 }
 
 const mapState = ({ currentUser }) => ({ currentUser })
+const mapDispatch = { logoutUser }
 
-export default connect(mapState)(Nav);
+export default connect(mapState, mapDispatch)(Nav);
