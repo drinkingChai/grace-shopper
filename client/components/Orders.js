b481245116aa5607d66bf6e0545c02cf1438f925
Wasif Zaman <teatocode@gmail.com>
2017-10-16 17:17:52 -0400

    refactor Orders

---
+++ client/components/Orders.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const formatDate = (_date) => {
+  const date = new Date(_date)
+  return `${date.getMonth()}/${date.getDate()}/${date.getYear()}`
+}
+
+export default function Orders ({ orders }) {
+  orders = orders.filter(order => order.id && !order.isCart)
+
+  return (
+    <div className="panel panel-default">
+      <h4 className='panel-heading'>Your Orders</h4>
+      <div className='panel-body'>
+        { orders.map(order => (
+          <div key={ order.id } className="panel panel-default">
+            <div className="panel-heading">
+              <strong>Placed on:</strong> { formatDate(order.updatedAt ) }
+              <label className='pull-right'><strong>Status:</strong> <kbd>{ order.status }</kbd></label>
+            </div>
+
+            <div className="panel-body">
+              <div className='table-responsive'>
+                <table className='table'>
+                  <thead>
+                    <tr>
+                      <th>Product</th>
+                      <th>Quantity</th>
+                      <th>Total</th>
+                    </tr>
+                  </thead>
+
+                  <tbody>
+                    {
+                      order.lineitems.map(lineitem => (
+                        <tr key={ lineitem.id }>
+                          <td>{ lineitem.product.name }</td>
+                          <td>{ lineitem.quantity }</td>
+                          <td>${ lineitem.quantity * lineitem.price }</td>
+                        </tr>
+                      ))
+                    }
+                  </tbody>
+                </table>
+              </div>
+              <hr/>
+              <label>Total:</label> ${ order.lineitems.reduce((total, item)=> (total += item.product.price * item.quantity), 0) }
+            </div>
+
+            <div className='panel-footer'>
+              <label>Delivered to:</label>
+              <br/>
+              { order.address }
+            </div>
+          </div>))}
+        </div>
+    </div>
+  )
+};
