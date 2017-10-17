7a65e4b01bba71e23e8ca0c96a3df860fcd7e4c4
Wasif Zaman <teatocode@gmail.com>
2017-10-16 22:11:22 -0400

    clean up UserAuth

--- client/components/UserAuth.js
+++ client/components/UserAuth.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { checkSession, loginUser, registerUser, logoutUser } from '../store';
+import { loginUser, registerUser } from '../store';
 
 class UserAuth extends Component {
   constructor() {
@@ -9,7 +9,6 @@ class UserAuth extends Component {
     this.onChangeHandler = this.onChangeHandler.bind(this);
     this.onLoginHandlher = this.onLoginHandlher.bind(this);
     this.onRegisterHandler = this.onRegisterHandler.bind(this);
-    this.onLogoutHandler = this.onLogoutHandler.bind(this);
   }
 
   onChangeHandler(ev) {
@@ -33,51 +32,37 @@ class UserAuth extends Component {
       .catch(err => console.log(err.message))
   }
 
-  onLogoutHandler(ev) {
-    ev.preventDefault()
-    this.props.logoutUser()
-      .then(() => this.props.history.push('/'))
-  }
-
   render() {
-    const { currentUser } = this.props
     const { email, password, name } = this.state
-    const { onChangeHandler, onLoginHandlher, onRegisterHandler, onLogoutHandler } = this
+    const { onChangeHandler, onLoginHandlher, onRegisterHandler } = this
 
     return (
       <div className="well">
-        { !currentUser.userId ?
-          <form onSubmit={ onLoginHandlher }>
-            <h4>Account</h4>
-            <div className="form-group">
-              <label htmlFor='email'>Email</label>
-              <input name='email' type='email' value={ email } onChange={ onChangeHandler } className="form-control" />
-            </div>
+        <form onSubmit={ onLoginHandlher }>
+          <h4>Account</h4>
+          <div className="form-group">
+            <label htmlFor='email'>Email</label>
+            <input name='email' type='email' value={ email } onChange={ onChangeHandler } className="form-control" />
+          </div>
 
-            <div className="form-group">
-              <label htmlFor='password'>Password</label>
-              <input name='password' type='password' value={ password } onChange={ onChangeHandler } className="form-control" />
-            </div>
-            <button className="btn btn-default">Login</button>
-            <hr/>
+          <div className="form-group">
+            <label htmlFor='password'>Password</label>
+            <input name='password' type='password' value={ password } onChange={ onChangeHandler } className="form-control" />
+          </div>
+          <button className="btn btn-default">Login</button>
+          <hr/>
 
-            <div className="form-group">
-              <label htmlFor='name'>Name</label>
-              <input name='name' value={ name } onChange={ onChangeHandler } className="form-control" />
-            </div>
-            <button onClick={ onRegisterHandler } className="btn btn-default">Register</button>
-          </form>
-          :
-          <form onSubmit={ onLogoutHandler }>
-            <h4>Welcome, { currentUser.name }!</h4>
-            <button className="btn btn-default">Logout</button>
-          </form> }
+          <div className="form-group">
+            <label htmlFor='name'>Name</label>
+            <input name='name' value={ name } onChange={ onChangeHandler } className="form-control" />
+          </div>
+          <button onClick={ onRegisterHandler } className="btn btn-default">Register</button>
+        </form>
       </div>
     )
   }
 }
 
-const mapState = ({ currentUser }) => ({ currentUser })
-const mapDispatch = { checkSession, loginUser, registerUser, logoutUser }
+const mapDispatch = { loginUser, registerUser }
 
-export default connect(mapState, mapDispatch)(UserAuth)
+export default connect(null, mapDispatch)(UserAuth)
