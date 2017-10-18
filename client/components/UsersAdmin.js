2d23cb15280944e183a03e1b0cd9d013c4cfd8c8
Wasif Zaman <teatocode@gmail.com>
2017-10-18 11:28:11 -0400

    map admin functions

--- client/components/UsersAdmin.js
+++ client/components/UsersAdmin.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import {
+  promoteUser, demoteUser,
+  enableUser, disableUser } from '../store'
 
-const UsersAdmin = ({ users }) => {
+const UsersAdmin = (props) => {
+  const {
+    users, currentUser,
+    promoteUser, demoteUser,
+    enableUser, disableUser } = props
+
+  console.log(props)
+
   return (
     <div>
       <h3>Users</h3>
@@ -11,10 +21,16 @@ const UsersAdmin = ({ users }) => {
           users.map(user => (
             <div key={ user.id }>
               { user.name }
-              <button>Make admin</button>
-              <button>Demote</button>
-              <button>Disable</button>
-              <button>Enable</button>
+
+              { user.id !== currentUser.userId ?
+                  <div>
+                    { user.isAdmin ?
+                        <button onClick={ () => demoteUser(user.id) }>Demote</button> :
+                        <button onClick={ () => promoteUser(user.id) }>Make admin</button> }
+                    { user.isDisabled ?
+                        <button onClick={ () => enableUser(user.id) }>Enable</button> :
+                        <button onClick={ () => disableUser(user.id) }>Disable</button> }
+                  </div> : null }
             </div>
           ))
         }
@@ -23,6 +39,10 @@ const UsersAdmin = ({ users }) => {
   )
 }
 
-const mapState = ({ users }) => ({ users })
+const mapState = ({ users, currentUser }) => ({ users, currentUser })
+const mapDispatch = {
+  promoteUser, demoteUser,
+  enableUser, disableUser
+}
 
-export default connect(mapState)(UsersAdmin)
+export default connect(mapState, mapDispatch)(UsersAdmin)
