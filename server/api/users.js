6382f663f48dbd5d9e09909a9dfdc6343db6a7a2
Wasif Zaman <teatocode@gmail.com>
2017-10-17 23:56:13 -0400

    user admin api

--- server/api/users.js
+++ server/api/users.js
@@ -1,9 +1,46 @@
 const users = require('express').Router()
 const { User } = require('../db').models
+const { requireAdmin } = require('./middlewares')
+
+users.get('/', requireAdmin, (req, res, next) => {
+  User.findUsers()
+    .then(users => res.send(users))
+    .catch(next)
+})
+
+users.post('/add-user', requireAdmin, (req, res, next) => {
+  User.createUser(req.body)
+    .then(() => res.sendStatus(201))
+    .catch(next)
+})
+
+users.put('/update-user/:id', requireAdmin, (req, res, next) => {
+  User.updateUser(req.params.id, req.body)
+    .then(() => res.sendStatus(200))
+    .catch(next)
+})
+
+users.put('/disable-user/:id', requireAdmin, (req, res, next) => {
+  User.updateUser(req.params.id, { isDisabled: true })
+    .then(() => res.sendStatus(200))
+    .catch(next)
+})
+
+users.put('/enable-user/:id', requireAdmin, (req, res, next) => {
+  User.updateUser(req.params.id, { isDisabled: false })
+    .then(() => res.sendStatus(200))
+    .catch(next)
+})
+
+users.delete('/delete-user/:id', requireAdmin, (req, res, next) => {
+  User.deleteUser(req.params.id)
+    .then(() => res.sendStatus(200))
+    .catch(next)
+})
 
 users.post('/', (req, res, next) => {
   User.createUser(req.body)
-    .then(() => res.sendStatus(202))
+    .then(() => res.sendStatus(201))
     .catch(next)
 })
 
