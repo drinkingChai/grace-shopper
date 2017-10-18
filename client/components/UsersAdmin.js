b0b84469babf5866ecc4eece66d1f5e0485505b6
Wasif Zaman <teatocode@gmail.com>
2017-10-18 10:16:15 -0400

    user buttons

--- client/components/UsersAdmin.js
+++ client/components/UsersAdmin.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-export default function () {
+const UsersAdmin = ({ users }) => {
   return (
     <div>
-      Users
+      <h3>Users</h3>
+
+      <div>
+        {
+          users.map(user => (
+            <div key={ user.id }>
+              { user.name }
+              <button>Make admin</button>
+              <button>Demote</button>
+              <button>Disable</button>
+              <button>Enable</button>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
+
+const mapState = ({ users }) => ({ users })
+
+export default connect(mapState)(UsersAdmin)
