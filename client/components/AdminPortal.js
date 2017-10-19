edf8d7207b518674971b414ec993ff54c165cb25
Wasif Zaman <teatocode@gmail.com>
2017-10-19 11:52:49 -0400

    valid statuses and searchable order admin

--- client/components/AdminPortal.js
+++ client/components/AdminPortal.js
@@ -6,7 +6,7 @@ import { fetchUsers, fetchAllOrders } from '../store'
 import AdminNav from './AdminNav'
 import UsersAdmin from './UsersAdmin'
 import ProductsAdmin from './ProductsAdmin'
-import OrdersAdmin from './OrdersAdmin'
+import SearchableOrdersAdmin from './SearchableOrdersAdmin'
 import Order from './Order'
 
 class AdminPortal extends Component {
@@ -16,7 +16,7 @@ class AdminPortal extends Component {
   }
 
   render() {
-    const { allOrders } = this.props
+    const { allOrders, currentUser } = this.props
 
     return (
       <div>
@@ -27,17 +27,20 @@ class AdminPortal extends Component {
 
         <Route path='/admin/users' component={ UsersAdmin } />
         <Route path='/admin/products' component={ ProductsAdmin } />
-        <Route exact path='/admin/orders' component={ OrdersAdmin } />
+        <Route exact path='/admin/orders' component={ SearchableOrdersAdmin } />
 
         {/* a bit messy, clean it up by having Order use fetch instead of filter? */}
         <Route exact path='/admin/orders/:id' render={ (ownProps) => (
-          <Order order={ allOrders.find(order => order.id == ownProps.match.params.id) }/>)}/>
+          <Order
+            order={ allOrders.find(order => order.id == ownProps.match.params.id) }
+            currentUser={ currentUser }/>
+        )}/>
       </div>
     )
   }
 }
 
-const mapState = ({ allOrders }) => ({ allOrders })
+const mapState = ({ allOrders, currentUser }) => ({ allOrders, currentUser })
 const mapDispatch = { fetchUsers, fetchAllOrders }
 
 export default connect(mapState, mapDispatch)(AdminPortal)
---
+++ client/components/helpers/validStatuses.js
@@ -0,0 +1,5 @@
+export default [
+  'CREATED', 'PROCESSING',
+  'SHIPPED', 'DELIVERED',
+  'CANCELLED'
+]
