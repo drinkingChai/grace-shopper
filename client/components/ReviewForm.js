5d4bd1704124ac083ecc935864db88b53e7529bc
panda <neptmanhall@gmail.com>
2017-10-19 13:46:46 -0400

    handled review restrictions

--- client/components/ReviewForm.js
+++ client/components/ReviewForm.js
@@ -11,10 +11,12 @@ class ReviewForm extends Component {
       title: '',
       blurb: '',
       date: {},
-      counter: 0
+      counter: 0,
+      error: ''
     };
     this.onChangeHandler = this.onChangeHandler.bind(this);
     this.onSubmitHandler = this.onSubmitHandler.bind(this);
+    this.handleFormRestrictions = this.handleFormRestrictions.bind(this);
   }
 
   onChangeHandler(ev) {
@@ -22,7 +24,12 @@ class ReviewForm extends Component {
     this.setState({[name]: value, counter: value.length });
 //    this.setState(Object.assign(this.state, { [name]: value}));
   }
-
+  
+  handleFormRestrictions(){
+    const { counter } = this.state;
+    let error = counter < 50 ? 'Review must be at least 50 characters...for some reason' : '';
+    this.setState({ error })
+  }
 //  componentWillReceiveProps(nextProps) {
 //    this.setState(nextProps);
 //  }
@@ -30,7 +37,8 @@ class ReviewForm extends Component {
   onSubmitHandler(ev) {
     ev.preventDefault();
     
-    
+    this.handleFormRestrictions()
+
     this.props.writeReview({
       productId: this.props.product.id, 
       userId: this.props.state.currentUser.id, 
@@ -38,28 +46,30 @@ class ReviewForm extends Component {
       blurb: this.state.blurb, 
       rating: this.state.rating,
       date: this.state.date,
-      counter: this.state.counter
     })
     .catch(err => console.log(err.message))
       
     this.setState({
-        rating: this.state.rating || 0,
-        title: '',
-        blurb: '',
-        date: {},
-        counter: 0
+      rating: this.state.rating || 0,
+      title: '',
+      date: {},
+      blurb: this.state.counter >= 50 ? '' : blurb,
+      counter: 0,
     });
   }
   
   render(){
     const {onChangeHandler, onSubmitHandler} = this
-    const { blurb, title, date, counter } = this.state
+    const { blurb, title, date, counter, error } = this.state
     const rating = [...Array(6).keys()];
     return (
       
         <div className="panel panel-primary">
           <h3 className="panel-heading"> Give a Review! </h3>
           <form onSubmit={ onSubmitHandler } className="form panel-body">
+            {
+              error.length ? <div className='alert alert-danger'>{ error }</div> : null
+            }
             <label> Rating: </label> 
             <select className="form-control" name="rating" onChange={ onChangeHandler }>
               {
@@ -70,7 +80,7 @@ class ReviewForm extends Component {
             <input className="form-control" name="title" type="text" value={ title } onChange={(event)=> this.setState({title:event.target.value})} placeholder="Title" autoFocus/>
             <label htmlFor="blurb"> Your Review </label>
             <textarea name="blurb" value={ blurb } onChange={ onChangeHandler} className="form-control blurb" />
-            <p className="counter">{ counter }/240</p>
+            <p className="counter">{ counter }</p>
             <button className="btn btn-success">Submit </button>
           </form>
         </div>
