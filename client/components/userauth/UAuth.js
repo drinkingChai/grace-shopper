81396f67b8d1cdcaf782c5284726e8ce3727b3d1
Wasif Zaman <teatocode@gmail.com>
2017-10-21 00:08:58 -0400

    UAuth replaces UserAuth

---
+++ client/components/userauth/UAuth.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { withRouter, Link } from 'react-router-dom'
+import { connect } from 'react-redux';
+import { loginUser } from '../../store';
+
+class UserAuth extends Component {
+  // can be presentational
+  constructor() {
+    super()
+    this.state = { email: '', password: '' };
+    this.onChange = this.onChange.bind(this);
+    this.onLogin = this.onLogin.bind(this);
+  }
+
+  onChange(ev) {
+    const { name, value } = ev.target
+    this.setState({ [name]: value })
+  }
+
+  onLogin(ev) {
+    ev.preventDefault()
+    const { email, password } = this.state
+    this.props.loginUser(email, password)
+      .then(() => this.props.history.goBack())
+      .catch(err => {
+        if (err.response.data == 'password change required') return this.props.history.push('/changepassword')
+      })
+  }
+
+  render() {
+    const { email, password } = this.state
+    const { onChange, onLogin } = this
+
+    return (
+      <div className='well'>
+        <form onSubmit={ onLogin }>
+          <h4>Account</h4>
+          <div className="form-group">
+            <label htmlFor='email'>Email</label>
+            <input name='email' type='email' value={ email } onChange={ onChange } className="form-control" />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor='password'>Password</label>
+            <input name='password' type='password' value={ password } onChange={ onChange } className="form-control" />
+          </div>
+          <button className="btn btn-default">Login</button>
+          <hr/>
+        </form>
+
+        <Link to='/register'>Register</Link>
+      </div>
+    )
+  }
+}
+
+const mapDispatch = { loginUser }
+
+export default withRouter(connect(null, mapDispatch)(UserAuth))
