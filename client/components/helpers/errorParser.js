69e4af7647f48c921878a219401e62cbc7d929c7
Wasif Zaman <teatocode@gmail.com>
2017-10-24 15:33:11 -0400

    also accepts strings

--- client/components/helpers/errorParser.js
+++ client/components/helpers/errorParser.js
@@ -1,3 +1,4 @@
 export default function (axiosErr) {
+  if (typeof axiosErr == 'string') return axiosErr
   return axiosErr.response.data.errors ? axiosErr.response.data.errors.reduce((a, d) => (`${a} ${d.message}`), '') : axiosErr.response.data
 }
