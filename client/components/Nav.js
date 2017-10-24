ee63ea92554ce145218a861cab20747fe82c9231
Burcu Noyan <burcunoyan@gmail.com>
2017-10-24 01:02:29 -0400

    refactor NavBar

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -3,26 +3,38 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { logoutUser } from '../store';
 
-const Nav = ({ currentUser, logoutUser }) => {
+const Nav = (props) => {
   return (
-    <nav className="navbar navbar-default">
+    <nav className="navbar navbar-default" id="navbar">
       <div className="container-fluid">
         <div className="navbar-header">
           <Link className="navbar-brand" to="/">GraceShopper</Link>
         </div>
-        <ul className="nav navbar-nav pull-right">
-          { currentUser.userId ?
-            <li><Link to="/account">My Account</Link></li> :
-            <li><Link to="/login">Log In</Link></li> }
-          { currentUser.userId && currentUser.isAdmin ?
-            <li><Link to="/admin">Admin</Link></li> : null }
-          { currentUser.userId ?
-            <li><Link to="/" onClick={ logoutUser }>Log out</Link></li> : null }
-        </ul>
+        <NavItems { ...props } className={ 'nav navbar-nav pull-right' } />
       </div>
     </nav>
-  )
-}
+  );
+};
+
+const NavItems = ({ currentUser, logoutUser, className }) => {
+  return (
+    <ul className={ className }>
+      {
+        currentUser.userId ?
+        <li><Link to="/account">My Account</Link></li> :
+        <li><Link to="/login">Log In</Link></li>
+      }
+      {
+        currentUser.userId && currentUser.isAdmin ?
+        <li><Link to="/admin">Admin</Link></li> : null
+      }
+      {
+        currentUser.userId ?
+        <li><Link to="/" onClick={ logoutUser }>Log out</Link></li> : null
+      }
+    </ul>
+  );
+};
 
 const mapState = ({ currentUser }) => ({ currentUser });
 const mapDispatch = { logoutUser };
