4f119ce5499b00545b540410597a9f0423386e50
Burcu Noyan <burcunoyan@gmail.com>
2017-10-23 23:01:25 -0400

    resize; move Register link; semicolons

--- client/components/userauth/UAuth.js
+++ client/components/userauth/UAuth.js
@@ -1,60 +1,57 @@
 import React, { Component } from 'react';
-import { withRouter, Link } from 'react-router-dom'
+import { withRouter, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { loginUser, setErrorAndClear } from '../../store';
 
 class UserAuth extends Component {
   // can be presentational
   constructor() {
-    super()
+    super();
     this.state = { email: '', password: '' };
     this.onChange = this.onChange.bind(this);
     this.onLogin = this.onLogin.bind(this);
   }
 
   onChange(ev) {
-    const { name, value } = ev.target
-    this.setState({ [name]: value })
+    const { name, value } = ev.target;
+    this.setState({ [name]: value });
   }
 
   onLogin(ev) {
-    ev.preventDefault()
-    const { email, password } = this.state
+    ev.preventDefault();
+    const { email, password } = this.state;
     this.props.loginUser(email, password)
       .then(() => this.props.history.goBack())
       .catch(err => {
-        if (err.response.data == 'password change required') return this.props.history.push('/changepassword')
-        this.props.setErrorAndClear(err.response.data)
+        if (err.response.data == 'password change required') return this.props.history.push('/changepassword');
+        this.props.setErrorAndClear(err.response.data);
       })
   }
 
   render() {
-    const { email, password } = this.state
-    const { onChange, onLogin } = this
+    const { email, password } = this.state;
+    const { onChange, onLogin } = this;
 
     return (
-      <div className='well'>
-        <form onSubmit={ onLogin }>
+      <div className="col-xs-12 col-sm-10 col-md-8">
+        <form className="well" onSubmit={ onLogin }>
           <h4>Account</h4>
           <div className="form-group">
-            <label htmlFor='email'>Email</label>
-            <input name='email' type='email' value={ email } onChange={ onChange } className="form-control" />
+            <label htmlFor="email">Email</label>
+            <input name="email" type="email" value={ email } onChange={ onChange } className="form-control" />
           </div>
 
           <div className="form-group">
-            <label htmlFor='password'>Password</label>
-            <input name='password' type='password' value={ password } onChange={ onChange } className="form-control" />
+            <label htmlFor="password">Password</label>
+            <input name="password" type="password" value={ password } onChange={ onChange } className="form-control" />
           </div>
-          <button className="btn btn-default">Login</button>
-          <hr/>
+          <button className="btn btn-default">Login</button> or <Link to="/register">Register</Link>
         </form>
-
-        <Link to='/register'>Register</Link>
       </div>
-    )
+    );
   }
 }
 
-const mapDispatch = { loginUser, setErrorAndClear }
+const mapDispatch = { loginUser, setErrorAndClear };
 
-export default withRouter(connect(null, mapDispatch)(UserAuth))
+export default withRouter(connect(null, mapDispatch)(UserAuth));
