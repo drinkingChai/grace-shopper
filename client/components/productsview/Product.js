ea19b2c8c265b7d510ddd7140c2fbd20a1f11de0
Burcu Noyan <burcunoyan@gmail.com>
2017-10-24 16:42:20 -0400

    add Cart

--- client/components/productsview/Product.js
+++ client/components/productsview/Product.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import ReviewForm from './ReviewForm';
 import { updateProduct } from '../../store';
+import ReviewForm from './ReviewForm';
 import ProductEditForm from '../admin/ProductEditForm';
+import Cart from '../shopping/Cart';
 
 class Product extends Component{
   constructor(props) {
@@ -37,65 +38,67 @@ class Product extends Component{
     if (!product) return <div>Product not found.</div>
 
     return (
-      <section>
+      <div>
+        <Cart />
         <div className="col-xs-12 col-sm-10">
-        <div className="col-xs-12 col-sm-6">
-          <img src={product.photo} width="100%" />
-        </div>
-
-        <div className="col-xs-12 col-sm-6">
-          <h2>{ product.name }</h2>
-          <br />
-          <h3><label>Categories:&nbsp;</label>
-           {
-            product.categories.map(category =>
-              <Link to={ `/categories/${ category.id }` } key={ category.id }><p>{category.name}</p></Link>)
-           }
-          </h3>
-          <h3 className="product-desc">{ product.description }</h3>
-        </div>
-        <div className="col-xs-12 col-sm-6">
-            <h4>${ product.price }</h4>
-            { currentUser.isAdmin ?
-            <div>
-              <button className="btn btn-default" onClick={ editFormVisible}> Edit Product </button>
-              { this.state.formVisible ?
-                 <ProductEditForm product={ product } allCategories={categories} /> : ''
-              }
-              </div> : ''
-            }
-        </div>
-        <div className="col-xs-10">
-          <div>
-            {
-              currentUser.userId ? <ReviewForm product={ product } /> : <p className="alert alert-primary"><Link to="/login">Login</Link> to leave a review.</p>
-            }
+          <div className="col-xs-12 col-sm-6">
+            <img src={product.photo} width="100%" />
           </div>
-          <div className="well">
-            <h3>Product Reviews:</h3>
-            <div>
-            {
-                  !product.reviews.length ?
-                    <p>No reviews.</p> :
-                    <p>Rating: {(product.reviews.map(review => review.rating).reduce((total, curr) => { return total + curr}, 0) / product.reviews.length).toFixed(1)} out of 5, with {product.reviews.length} reviews.
-                    </p>
+
+          <div className="col-xs-12 col-sm-6">
+            <h2>{ product.name }</h2>
+            <br />
+            <h3><label>Categories:&nbsp;</label>
+             {
+              product.categories.map(category =>
+                <Link to={ `/categories/${ category.id }` } key={ category.id }><p>{category.name}</p></Link>)
+             }
+            </h3>
+            <h3 className="product-desc">{ product.description }</h3>
+          </div>
+          <div className="col-xs-12 col-sm-6">
+              <h4>${ product.price }</h4>
+              { currentUser.isAdmin ?
+              <div>
+                <button className="btn btn-default" onClick={ editFormVisible}> Edit Product </button>
+                { this.state.formVisible ?
+                   <ProductEditForm product={ product } allCategories={categories} /> : ''
+                }
+                </div> : ''
               }
+          </div>
+          <div className="col-xs-10">
+            <div>
               {
-                product.reviews.map(review => {
-                  return (
-                    <article className="reviews" key={review.id}>
-                      <p className="rating">Rating: {review.rating}/5</p>
-                      <p className="byline">By {review.user.name} on {new Date().toUTCString()} </p>
-                      <p className="title">{review.title}</p>
-                      <p className="blurb">{review.blurb}</p>
-                    </article>
-                  )
-                })
+                currentUser.userId ? <ReviewForm product={ product } /> : <p className="alert alert-primary"><Link to="/login">Login</Link> to leave a review.</p>
               }
             </div>
+            <div className="well">
+              <h3>Product Reviews:</h3>
+              <div>
+                {
+                  !product.reviews.length ?
+                      <p>No reviews.</p> :
+                      <p>Rating: {(product.reviews.map(review => review.rating).reduce((total, curr) => { return total + curr}, 0) / product.reviews.length).toFixed(1)} out of 5, with {product.reviews.length} reviews.
+                      </p>
+                }
+                {
+                  product.reviews.map(review => {
+                    return (
+                      <article className="reviews" key={review.id}>
+                        <p className="rating">Rating: {review.rating}/5</p>
+                        <p className="byline">By {review.user.name} on {new Date().toUTCString()} </p>
+                        <p className="title">{review.title}</p>
+                        <p className="blurb">{review.blurb}</p>
+                      </article>
+                    )
+                  })
+                }
+              </div>
+            </div>
           </div>
-        </div> </div>
-      </section>
+        </div>
+      </div>
     );
   }
 }
