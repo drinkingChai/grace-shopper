a9c27075ee9da4f9cb3e18ab361e7ed944e0f257
Burcu Noyan <burcunoyan@gmail.com>
2017-10-24 04:30:35 -0400

    guest login style

--- client/components/userauth/CheckOutLogin.js
+++ client/components/userauth/CheckOutLogin.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 // Components
 import UAuth from './UAuth'
 
 const CheckLogin = ({ currentUser, ownProps }) => {
-  if (currentUser.userId) ownProps.history.push('/checkout')
+  if (currentUser.userId) ownProps.history.push('/checkout');
 
   return (
-    <div className="well">
-      <div className="col-xs-12 col-md-6">
+    <div className="row">
+      <div className="col-xs-12 col-sm-8">
         <UAuth />
-      </div>
-      <div className='col-md-6'>
-        <Link to='/checkout?guest=1'>Continue as Guest</Link>
+        <div className="col-xs-12 col-sm-4" id="guest">
+          <Link to="/checkout?guest=1" className="btn btn-default form-control">Continue as Guest</Link>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 const mapState = ({ currentUser }, ownProps) => ({
   currentUser,
   ownProps
-})
+});
 
-export default connect(mapState)(CheckLogin)
+export default connect(mapState)(CheckLogin);
