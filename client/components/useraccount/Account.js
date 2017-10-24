06603c318932a247ae36766995e4587ea620d5bc
Burcu Noyan <burcunoyan@gmail.com>
2017-10-24 04:57:53 -0400

    styles

--- client/components/useraccount/Account.js
+++ client/components/useraccount/Account.js
@@ -1,27 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import Orders from '../Orders'
-import AccountUpdateForm from './AccountUpdateForm'
+import Orders from '../Orders';
+import AccountUpdateForm from './AccountUpdateForm';
 
 const Account = ({ currentUser, orders }) => {
-  orders = orders.filter(order => order.id && !order.isCart)
+  orders = orders.filter(order => order.id && !order.isCart);
 
   return (
-    <div>
-      <h2>Welcome, { currentUser.name }</h2>
-      <hr/>
-
-      <div className="panel panel-default">
-        <h4 className='panel-heading'>Account Details</h4>
-        <div className='panel-body'>
-          <AccountUpdateForm user={ currentUser }/>
+    <div className="col-xs-12">
+      <h3>Welcome, { currentUser.name }!</h3>
+      <hr />
+      <div className="row">
+        <div className="col-xs-12 col-sm-4">
+          <div className="panel panel-default">
+            <h4 className="panel-heading">Account Details</h4>
+            <div className="panel-body">
+              <AccountUpdateForm user={ currentUser } />
+            </div>
+          </div>
+        </div>
+        <div className="col-xs-12 col-sm-8">
+          <div className="panel panel-default">
+            <h4 className="panel-heading">Orders</h4>
+            <div className="panel-body">
+              <Orders orders={ orders } />
+            </div>
+          </div>
         </div>
       </div>
-
-      <Orders orders={ orders } />
     </div>
-  )
+  );
 };
 
 const mapState = ({ currentUser, orders }) => ({ currentUser, orders });
